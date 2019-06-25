895cf19aa7dc21c761d373f861944510669d8b12
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-06-25 15:45:49 +0530

    Create con.py

---
+++ con.py
@@ -0,0 +1,8 @@
+l=input("Input a letter of a albhabet:")
+if l in('a','e','i','o','u'):
+  print("%s is a vowel:" % l)
+elif l=='y':
+  print("sometimes letter y stand for vowel ,sometimes stand for consonant:")
+else:
+  print("%s is a consonant:" % l)
+  
